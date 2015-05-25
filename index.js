3186602f3b0f43e70004274fc2d5b0f885e64b0e
Ammly <ammlyf@gmail.com>
2015-05-25 14:03:23 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var Region = function (regionName) {
-       this.name = regionName; 
-   }; 
-   var Country = function (countryName) {
-       this.name = countryName;
-   } 
-   Country.prototype = new Region("A random region");
-   var country = new Country("A random country");
-   console.log(country.name);
